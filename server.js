033ea547ff4b3d18c8ef174075ad50a6541ed54b
Joe Mulick <joemulick@gmail.com>
2017-06-03 18:04:42 -0700

    Scraper scraping correctly. Took out mongojs, added mongoose

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // Dependencies
 var express = require("express");
-var mongojs = require("mongojs");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var express = require("express");
 var request = require("request");
 var cheerio = require("cheerio");
+var mongoose = require("mongoose");
+
+var Article = require("./models/Article.js");
 
 var app = express();
 
@@ -16,12 +18,20 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-// Database configuration
-var databaseUrl = "scraper";
-var collections = ["items"];
+// Database configuration with mongoose
+mongoose.connect("mongodb://localhost/internetScrape");
+var db = mongoose.connection;
 
-// Hook mongojs config to db variable
-var db = mongojs(databaseUrl, collections);
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
 
 // Log any mongojs errors to console
 db.on("error", function(error) {
@@ -42,7 +52,43 @@ app.get("/items", function(req, res) {
 
 });
 
+// A GET request to scrape the echojs website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  request("https://techcrunch.com/", function(error, response, html) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(html);
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("div.block-content").each(function(i, element) {
 
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this).children("h2").text();
+      result.text = $(this).children("p").text();
+
+      // Using our Article model, create a new entry
+      // This effectively passes the result object to the entry (and the title and link)
+      var entry = new Article(result);
+
+      // Now, save that entry to the db
+      entry.save(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        // Or log the doc
+        else {
+          console.log(doc);
+        }
+      });
+
+    });
+  });
+  // Tell the browser that we finished scraping the text
+  res.send("Scrape Complete");
+});
 
 
 
